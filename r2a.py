8ed6129586ecb7a621ff3306e9c8dc5aa2cc56d4
Serdiuk Roman <serdiuk.r@gmail.com>
2018-03-25 21:57:29 +0300

    add converter from arabic to roman

--- r2a.py
+++ r2a.py
@@ -2,46 +2,91 @@
 # -*- coding: utf-8 -*-
 
 
-ROMAN_NUMBERS = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+import re
 
 
-def roman_to_arabic (numb):
-    i = 0
+def araic_to_roman(numb):
+    arab = [1000, 900, 500, 400, 100, 90,
+            50, 40, 10, 9, 5, 4, 1]
+    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC',
+             'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    index = 0
+    res = ""
+    while True and index < len(arab):
+        if numb >= arab[index]:
+            res = res + roman[index]
+            numb -= arab[index]
+        else:
+            index += 1
+    if valid_roman(res):
+        print("\t{}".format(res))
+    else:
+        print("Error")
+
+
+def roman_to_arabic(numb):
+    roman = ['CM', 'M', 'CD', 'D', 'XC', 'C',
+             'XL', 'L', 'IX', 'X', 'IV', 'V', 'I']
+    arab = [900, 1000, 400, 500, 90, 100,
+            40, 50, 9, 10, 4, 5, 1]
+    index = 0
     res = 0
-    roman = ['CM', 'M', 'CD', 'D', 'XC', 'C', 'XL', 'L', 'IX', 'X', 'IV', 'V', 'I']
-    arab =  [ 900, 1000, 400, 500,  90,  100,  40,   50,   9,   10,   4,   5,   1 ]
+    while True and index < len(roman):
+        if roman[index] in numb:
+            res += arab[index]
+            numb = numb.replace(roman[index], '', 1)
+        else:
+            index += 1
+    print("\t{}".format(res))
+
+
+def valid_roman(numb):
+    tpl = '^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$'
+    if re.match(tpl, numb) is not None:
+        return True
+    else:
+        return False
+
+
+def test_roman_num():
+    roman_to_arabic('I')  # 1
+    roman_to_arabic('II')  # 2
+    roman_to_arabic('III')  # 3
+    roman_to_arabic('IV')  # 4
+    roman_to_arabic('V')  # 5
+    roman_to_arabic('VI')  # 6
+    roman_to_arabic('VII')  # 7
+    roman_to_arabic('VIII')  # 8
+    roman_to_arabic('IX')  # 9
+    roman_to_arabic('X')  # 10
+    roman_to_arabic('XXXI')  # 31
+    roman_to_arabic('XLVI')  # 46
+    roman_to_arabic('XCIX')  # 99
+    roman_to_arabic('DLXXXIII')  # 583
+    roman_to_arabic('DCCCLXXXVIII')  # 888
+    roman_to_arabic('MDCLXVIII')  # 1668
+    roman_to_arabic('MCMLXXXIX')  # 1989
+    roman_to_arabic('MMX')  # 2010
+    roman_to_arabic('MMXI')  # 2011
+    roman_to_arabic('MMXII')  # 2012
+    roman_to_arabic('MMMCMXCIX')  # 3999
+
+
+def main():
+    print("""Введіть число від 1 до 3999, римськими або арабськими,
+або натисніть клавішу 'Enter', щоб закіничити програму""")
     while True:
-        try:
-            # print("i=", i,"\tr=", roman[i], "\tnumb=", numb, "\tres=", res)
-            if roman[i] in numb:
-                res += arab[i]
-                numb = numb.replace(roman[i], '', 1)
-            else:
-                i += 1
-        except:
-            break
-    return res
-
-
-def valid_roman (numb):
-    
-    for i in numb:
-        if i not in ROMAN_NUMBERS:
-            return False
-    return True
-
-
-def main ():
-    while True:
-        elem = (input("Введіть ціле число від 1 до 3999 римськими числами\n\t")).upper()
+        elem = (input("-->\t")).upper()
         if not elem:
             break
-        if valid_roman(elem):
-            res = roman_to_arabic(elem)
-            print("\t{}".format(res))
+        elif elem.isnumeric() and 0 < int(elem) < 4000:
+            araic_to_roman(int(elem))
+        elif valid_roman(elem):
+            roman_to_arabic(elem)
+        elif elem == "TEST":
+            test_roman_num()
         else:
-            print("Римські числа:", *ROMAN_NUMBERS)
-        
+            print("Виконайте початкові умови")
 
 
 if __name__ == '__main__':
