8690c1e9c44f33a24b8790681a63ed61ae0edcb1
Serdiuk Roman <serdiuk.r@gmail.com>
2018-05-02 20:09:32 +0300

    less3

--- L2/DZ2/triangle_serpinskii.py
+++ L2/DZ2/triangle_serpinskii.py
@@ -1,30 +1,33 @@
-from turtle import *
-import time
-import random
-
-
-coordinate_list = []
-speed(10)
+from turtle import (
+    speed, color, begin_fill, penup, goto, pendown, fd,
+    left, pos, end_fill, right, tracer,
+)
 
 
 def new_coordinate(x, y, base, level):
-    if level > 1:
-        for i in range(1, level):
-            new_base = base / (2 ** i)
-            new_x = x
-            new_level = level - i
-            if new_level > 0:
-                for j in range(round(base) // round(new_base) - 1):
-                    new_x += new_base
-                    if j % 2 == 0:
-                        coordinate_list.append((
-                            new_x,
-                            y,
-                            new_base,
-                            new_level))
+    """calculate coordinates"""
+    if level <= 0:
+        return
+
+    for i in range(1, level):
+        new_base = base / (2 ** i)
+        new_x = x
+        new_level = level - i
+
+        if new_level <= 0:
+            break
+
+        for j in range(round(base) // round(new_base) - 1):
+            new_x += new_base
+
+            if j % 2 != 0:
+                break
+
+            next_triangle(new_x, y, new_base, new_level)
 
 
 def first_triangle(x, y, base, level):
+    """Make first triangle"""
     color("black", "black")
     begin_fill()
     penup()
@@ -43,10 +46,10 @@ def first_triangle(x, y, base, level):
 
 
 def next_triangle(x, y, base, level):
+    """Make all triangle but first"""
     color("white", "white")
     begin_fill()
     penup()
-    # print("y = ", round(y))
     goto(x, y)
     pendown()
     left(60)
@@ -62,13 +65,27 @@ def next_triangle(x, y, base, level):
     new_coordinate(x2, y2, base, level)
 
 
-def triangle_serpinskii(base, level):
+def make_triangle_serpinskii():
+    """Main fun"""
+    base = input("please enter base: ")
+
+    if base == "":
+        base = 400
+    else:
+        base = int(base)
+
+    level = input("please enter level: ")
+
+    if level == "":
+        level = 5
+    else:
+        level = int(level)
+
+    speed(0)
+    tracer(0)
     first_triangle(0, -base / 2, base, level)
-    while coordinate_list:
-        new_p = coordinate_list.pop(0)
-        next_triangle(*new_p)
 
 
-triangle_serpinskii(400, 7)
-time.sleep(2)
-# input()
+if __name__ == "__main__":
+    make_triangle_serpinskii()
+    input()
---
+++ L3/lec_oop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+class AnimalError(Exception):
+    def __init__(self, animal):
+        super().__init__()
+        self.animal = animal
+
+
+
+class Animal:
+    """Base class for animal
+
+    bla-bla-car
+    """
+    def say(self):
+        raise NotImplementetError
+
+    def __init__(self, name):
+        self.name = name
+
+
+def butter(fn):
+    def wrapper(self, msg):
+        print("see butter")
+        fn(self, msg)
+    return wrapper
+
+
+class Predator:
+    def say(self):
+        print("Rrrrr")
+
+
+class Cat(Animal, Predator):
+    """sss"""
+    def __init__(self, name, color = "red"):
+        self.color = color
+        super().__init__(name)
+
+    def __str__(self):
+        return "error: %s" % str(self.name)
+
+    @butter
+    def say(self, msg):
+        if msg != "":
+            print("Cat", self.name, "Meove", msg)
+        else:
+            raise AnimalError(self)
+
+
+
+
+black_cat = Cat("Black")
+vasia_car = Cat("Vasia", "brown")
+sharik_car = Cat("Sharik", "white")
+black_cat.say("qwe")
+vasia_car.say("rty")
+sharik_car.say("asd")
+
+try:
+    sharik_car.say("")
+except AnimalError as e:
+    print(e.animal)
