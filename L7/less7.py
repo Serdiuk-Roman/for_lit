2067cca5d4f9f325159b18f4105999b32a3135d5
Serdiuk Roman <serdiuk.r@gmail.com>
2018-05-19 13:00:33 +0300

    less7

---
+++ L7/less7.py
@@ -0,0 +1,9 @@
+# min max принимает функию key которой можна
+# передать функцию которая возращает значения
+# вар 2 переопределить функции __gt__(self, obj)
+# __lt__, __ea__, __ge__, __ie__, __ne__
+# a < b ==> a.__lt__(b)
+# __gt__(self, army):
+#     return self.heath > army.heath
+# method filter(func, arg)
+# mod statistic 
