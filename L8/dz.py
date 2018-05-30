90b080d0a5e01f80cf11de5e12be31f96ded568d
Serdiuk Roman <serdiuk.r@gmail.com>
2018-05-30 20:17:21 +0300

    add less10

--- L8/dz.py
+++ L8/dz.py
@@ -4,12 +4,21 @@
 class StringCalculator:
 
     def add(self, number_str):
+        print()
+        print(number_str)
         number_str = number_str.replace(",", " ")
         number_str = number_str.replace("\n", " ")
-        print(number_str)
         if "//" in number_str[:2]:
+            separ = ""
             number_str = number_str.replace("//", "")
-            number_str = number_str.replace("#", " ")
+            for char in number_str:
+                if char.isdigit():
+                    break
+                else:
+                    separ = separ + char
+            print(separ)
+            number_str = number_str.replace(separ, " ")
+            print(number_str)
         number_list = number_str.split()
         if "-" in number_str:
             negatives = [
@@ -20,15 +29,26 @@ class StringCalculator:
             return "отрицательные числа запрещены: {}".format(
                 ",".join(negatives)
             )
-
+        print(number_list)
         res = [
-            int(x)
-            for x in number_list
-            if 0 < int(x) < 1000
+            char
+            for char in number_list
+            if len(char) > 0
         ]
+        res = map(int, number_list)
         return sum(res)
 
 
 if __name__ == "__main__":
     g = StringCalculator()
     g.add("5 3, 2")
+    g.add("5 3 2")
+    g.add("")
+    g.add("1")
+    g.add("10,20")
+    g.add("1\n2")
+    g.add("1\n2,3\n4")
+    g.add("-1,2,-3")
+    g.add("1000,2,3000")
+    g.add("//#\n1#2")
+    g.add("//###\n1###2")
