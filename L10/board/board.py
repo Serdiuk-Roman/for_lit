86d12334949e025cc451097b69a421813775f405
Serdiuk Roman <serdiuk.r@gmail.com>
2018-06-08 00:13:34 +0300

    done werkzeug board

--- L10/board/board.py
+++ L10/board/board.py
@@ -2,27 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 import redis
-from urllib.parse import urlparse
+from datetime import datetime
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 from jinja2 import Environment, FileSystemLoader
-
-
-def is_valid_url(url):
-    parts = urlparse(url)
-    return parts.scheme in ('http', 'https')
-
-
-def is_valid_creator(name):
-    return 0 < len(name) < 30
-
-
-def is_valid_board_text(text):
-    return 0 < len(text) < 50
 
 
 def base36_encode(number):
@@ -37,7 +25,7 @@ def base36_encode(number):
 
 
 class Board():
-    """docstring for Shortly"""
+    """docstring for Board"""
 
     def __init__(self, config):
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
@@ -53,11 +41,18 @@ class Board():
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
+    def error_404(self):
+        response = self.render_template('404.html')
+        response.status_code = 404
+        return response
+
     def dispatch_request(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
             endpoint, values = adapter.match()
             return getattr(self, 'on_' + endpoint)(request, **values)
+        except NotFound as e:
+            return self.error_404()
         except HTTPException as e:
             return e
 
@@ -69,7 +64,18 @@ class Board():
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
-    # def insert_board(self, url):
+    def insert_board(self, values):
+        board_num = self.redis.incr('last-board-id')
+        board_id = base36_encode(board_num)
+        self.redis.set('board:' + board_id, values[0])
+        self.redis.set('creator:board:' + board_id, values[1])
+
+        dt = str(datetime.now()).split('.')[0]
+        self.redis.set(
+            'datetime:board:' + board_id,
+            dt
+        )
+        return board_id
     #     short_id = self.redis.get('reverse-url:' + url)
     #     if short_id is not None:
     #         return short_id
@@ -79,48 +85,94 @@ class Board():
     #     self.redis.set('reverse-url:' + url, short_id)
     #     return short_id
 
+    def insert_comment(self, values, board_id):
+        comments = self.redis.get('comments:board:' + board_id)
+        if comments is None:
+            json_values = json.dumps([values])
+            self.redis.set('comments:board:' + board_id, json_values)
+            return
+        unpacked_comments = json.loads(comments.decode('utf-8'))
+        unpacked_comments.append(values)
+        insert_value = json.dumps(unpacked_comments)
+        self.redis.set('comments:board:' + board_id, insert_value)
+
     def on_base(self, request):
-        error = ''
+
+        error = None
         boards = []
         values = []
+
+        keys = self.redis.keys('board:*')
+        keys = [
+            key.decode('utf-8')
+            for key in keys
+        ]
+        keys.sort()
+
+        boards = self.redis.mget(keys)
+        boards = [
+            board.decode('utf-8')
+            for board in boards
+        ]
+
         if request.method == 'POST':
             creator = request.form['creator']
             board_name = request.form['board_name']
-            values = [creator, board_name]
-            if not is_valid_creator(creator):
-                error += 'Username max 30\n'
-            elif not is_valid_board_text(board_name):
-                error += 'Board_name max 50\n'
+            values = [board_name, creator]
+            if not 0 < len(creator) <= 30:
+                error = 'Username max 30'
+            elif not 0 < len(board_name) <= 50:
+                error = 'Board_name max 50'
             else:
-                # short_id = self.insert_url(url)
-                return redirect('/')
+                board_id = self.insert_board(values)
+                return redirect('/board:{}'.format(board_id))
+
         return self.render_template(
             'board.html',
             error=error,
+            keys=keys,
             boards=boards,
             values=values
         )
 
-    def on_follow_short_link(self, request, short_id):
-        link_target = self.redis.get('url-target:' + short_id)
-        if link_target is None:
-            raise NotFound()
-        self.redis.incr('click-count:' + short_id)
-        return redirect(link_target)
+    def on_board_detail(self, request, board_id):
 
-    def on_short_link_details(self, request, short_id):
-        link_target = self.redis.get('url-target:' + short_id)
-        if link_target is None:
-            raise NotFound()
-        click_count = int(self.redis.get('click-count:' + short_id) or 0)
+        error = None
+        values = []
+
+        try:
+            board = self.redis.get('board:' + board_id).decode('utf-8')
+        except AttributeError:
+            return self.error_404()
+        creator = self.redis.get('creator:board:' + board_id).decode('utf-8')
+        dt = self.redis.get('datetime:board:' + board_id).decode('utf-8')
+
+        if request.method == 'POST':
+            comment_creator = request.form['comment_creator']
+            comment = request.form['comment']
+            values = [comment, comment_creator]
+            if not 0 < len(comment_creator) <= 30:
+                error = 'Username max 30'
+            elif not 0 < len(comment) <= 255:
+                error = 'Comment max 255'
+            else:
+                self.insert_comment(values, board_id)
+                values = []
+        try:
+            comments = self.redis.get('comments:board:' + board_id)
+            unpacked_comments = json.loads(comments.decode('utf-8'))
+        except AttributeError:
+            unpacked_comments = []
+
         return self.render_template(
-            'short_link_details.html',
-            link_target=link_target,
-            short_id=short_id,
-            click_count=click_count
+            'board_details.html',
+            board=board,
+            creator=creator,
+            dt=dt,
+            error=error,
+            values=values,
+            comments=unpacked_comments
         )
-
-    
 
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
