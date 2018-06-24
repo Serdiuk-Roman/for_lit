70236625e2a0df1a89f1f62b3f4a57b3c51e4c38
Serdiuk Roman <serdiuk.r@gmail.com>
2018-06-24 09:49:59 +0300

    del dr

--- for_/shortly_proj/shortly_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from shortly_app.models import Snippet, Shortly
-
-admin.site.register(Snippet)
-admin.site.register(Shortly)
--- for_/shortly_proj/shortly_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShortlyAppConfig(AppConfig):
-    name = 'shortly_app'
